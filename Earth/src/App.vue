b123ff8117de3c9b869cd44739976f51f9a484ac
yuchenzzz1013 <1592176626@qq.com>
2024-01-15 10:41:21 +0800

    1
    1

---
+++ Earth/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Earthdata/>
+  </div>
+</template>
+
+<script>
+import Earthdata from './components/Earthdata.vue'
+export default {
+  name: 'App',
+  components: {
+  Earthdata
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
