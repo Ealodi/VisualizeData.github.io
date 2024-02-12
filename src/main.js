8357a135cb7f1820aebcce0b0fb8dcf88e2b9877
Ealodi <75173518+Ealodi@users.noreply.github.com>
2024-02-12 13:56:24 +0800

    1

--- src/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-import './assets/main.css';
-import { createApp } from 'vue';
-import App from './App.vue';
-import WordCloud from './components/wordcloud.vue';
-
-let app = createApp(App);
-app.component('wordcloud', WordCloud);  // 注册 WordCloud 组件
-app.mount("#char4");
